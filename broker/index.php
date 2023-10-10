00711ae55376d1e88382315c800edfee438675de
HELPER\Helper <komay5@bk.ru>
2023-10-10 13:26:00 -0400

    SQL Queries for the property management page + associated JS functions + random properties data

--- broker/index.php
+++ broker/index.php
@@ -10,4 +10,4 @@
     </body>
 </html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="main.js"></script>
+<script src="import_data.js"></script>
--- general/MySqlRequests.php
+++ general/MySqlRequests.php
@@ -42,9 +42,100 @@ $RECIPES = [
                             PASSWORD = '?-?',
                             STATUS = '?-?'
                         WHERE USER_ID = '?-?';",
+    
     'updateBrokerStatus' => "UPDATE luckyseven.tbl_user
                             SET STATUS = '?-?'
-                            WHERE USER_ID = '?-?';"
+                            WHERE USER_ID = '?-?';",
+    /*
+     * Properties Mangement
+     */
+        'getBrokerProperties' => "
+        SELECT
+		P.PROPERTY_ID,
+		PT.NAME AS PROPERTY_TYPE,
+		L.NAME AS LOCATION_NAME,
+		L.CITY AS LOCATION_CITY,
+		L.PROVINCE AS LOCATION_PROVINCE,
+		L.COUNTRY AS LOCATION_COUNTRY,
+		P.DESCRIPTION,
+		P.ADDRESS,
+		P.YEAR,
+		P.PARKING_COUNT,
+		P.BATH_COUNT,
+		P.ROOMS_COUNT,
+		P.PRICE,
+		PI.IMG AS COVER_IMAGE,
+		P.IS_FOR_SALE
+	FROM
+		luckyseven.tbl_property P
+	LEFT JOIN
+		luckyseven.tbl_property_type PT ON P.TYPE_ID = PT.TYPE_ID
+	LEFT JOIN
+		luckyseven.tbl_location L ON P.AREA_ID = L.AREA_ID
+	LEFT JOIN
+		luckyseven.tbl_property_img PI ON P.COVER_IMG_ID = PI.IMG_ID
+	WHERE BROKER_ID = '?-?';",
+    /*
+     * Properties Search
+     */
+    'getProperties' => "
+        SELECT
+		P.PROPERTY_ID,
+		PT.NAME AS PROPERTY_TYPE,
+		L.NAME AS LOCATION_NAME,
+		L.CITY AS LOCATION_CITY,
+		L.PROVINCE AS LOCATION_PROVINCE,
+		L.COUNTRY AS LOCATION_COUNTRY,
+		P.DESCRIPTION,
+		P.ADDRESS,
+		P.YEAR,
+		P.PARKING_COUNT,
+		P.BATH_COUNT,
+		P.ROOMS_COUNT,
+		P.PRICE,
+		PI.IMG AS COVER_IMAGE,
+		P.IS_FOR_SALE
+	FROM
+		luckyseven.tbl_property P
+	LEFT JOIN
+		luckyseven.tbl_property_type PT ON P.TYPE_ID = PT.TYPE_ID
+	LEFT JOIN
+		luckyseven.tbl_location L ON P.AREA_ID = L.AREA_ID
+	LEFT JOIN
+		luckyseven.tbl_property_img PI ON P.COVER_IMG_ID = PI.IMG_ID
+	WHERE
+		(P.AREA_ID = '?-?' OR '?-?' = -1)
+		AND (P.PRICE <= '?-?' OR '?-?' = -1)
+		AND (P.IS_FOR_SALE = '?-?' OR '?-?' = -1);",
+    'getPropertyTypes' => 'SELECT * FROM luckyseven.tbl_property_type;',
+    'getLocations' => 'SELECT * FROM luckyseven.tbl_location',
+    
+    'insertNewProperty' => "INSERT INTO luckyseven.tbl_property (PROPERTY_ID, BROKER_ID, DESCRIPTION, AREA_ID, ADDRESS, POSTAL, YEAR,
+            PARKING_COUNT, BATH_COUNT, ROOMS_COUNT, TYPE_ID, PRICE, IS_FOR_SALE, STATUS)
+            VALUES (SELECT IFNULL(MAX(PROPERTY_ID), 0) + 1, '?-?', '?-?', '?-?', '?-?', '?-?', '?-?', '?-?', '?-?', '?-?', '?-?', '?-?', '?-?', 1);",
+    
+    'updateProperty' => "UPDATE luckyseven.tbl_property
+                            SET
+                                COVER_IMG_ID = '?-?',
+                                DESCRIPTION = '?-?',
+                                AREA_ID = '?-?',
+                                ADDRESS = '?-?',
+                                POSTAL = '?-?',
+                                YEAR = '?-?',
+                                PARKING_COUNT = '?-?',
+                                BATH_COUNT = '?-?',
+                                ROOMS_COUNT = '?-?',
+                                TYPE_ID = '?-?',
+                                PRICE = '?-?',
+                                IS_FOR_SALE = '?-?',
+                                STATUS = '?-?'
+                            WHERE
+                                PROPERTY_ID = '?-?';",
+    'linkImageToProperty' => "INSERT INTO luckyseven.tbl_property_img (IMG_ID, IMG, PROPERTY_ID)
+                                VALUES (SELECT IFNULL(MAX(IMG_ID), 0) + 1, '?-?', '?-?');",
+    'getPropertyImages' => "SELECT IMG_ID, IMG
+                            FROM tbl_property_img
+                            WHERE PROPERTY_ID = '?-?';"
 ];
 
 $importedRecipeKey = $_POST['recipeKey']; //the key of the SQL recipe
