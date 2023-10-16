c11574f8df03b0800b53e69ac74a64682f4d6eb3
Gderhy <gabrielderhy@gmail.com>
2023-10-16 12:59:43 -0400

    Continuing implementing the navbar

--- broker/properties-mng/my-listings.php
+++ broker/properties-mng/my-listings.php
@@ -4,22 +4,21 @@
     <head>
         <meta charset="UTF-8">
 
-        <link rel="stylesheet" href="../Navbar/Css/navStyle.css">
-        <link rel="stylesheet" href="Css/Table.css">
-        <link rel="stylesheet" href="Css/dashboard-listings.css">
+        <link rel="stylesheet" href="/Navbar/Css/navStyle.css">
+        <link rel="stylesheet" href="/broker/Css/Table.css">
+        <link rel="stylesheet" href="/broker/Css/dashboard-listings.css">
 
-        <title>Broker</title>
+        <title>My Listings  </title>
     </head>
     <body>
-
-    <header>
-        <nav id="navbar"></nav>
-    </header>
+        <header>
+            <nav id="navbar"></nav>
+        </header>
 
 <!--        Make sure user is logged in and has role-->
     <div class="broker" >
         <div class="dashboard-listings">
-            <h1>My listings</h1>
+            <h1>My Listings</h1>
             <table class="dashboard-table" id="table-listings-id">
                 <thead class="dashboard-thead">
                 <tr class="dashboard-row">
@@ -45,10 +44,10 @@
 
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="JavaScript/import_data.js"></script>
-    <script src="JavaScript/Login_Authentication.js" type="module"></script>
+    <script src="/broker/JavaScript/import_data.js"></script>
+    <script src="/broker/JavaScript/Login_Authentication.js" type="module"></script>
 
-    <script src="../Navbar/navbar.js"></script>
-    <script>implementNavbar("Broker")</script>
+    <script src="/Navbar/navbar.js"></script>
+    <script>implementNavbar("My Listings")</script>
     </body>
 </html>
