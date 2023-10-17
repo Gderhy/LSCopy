3823601d29f1951b995b95ad201f311bc6b1fee2
Gderhy <gabrielderhy@gmail.com>
2023-10-17 12:30:33 -0400

    I connected my listing table to fetch the listing data from db

--- broker/properties-mng/PopulateMyListingTable.js
+++ broker/properties-mng/PopulateMyListingTable.js
@@ -91,6 +91,7 @@ function getTableRow(listing){
     td_PROPERTY_NAME.id = `PROPERTY_NAME-${listing.PROPERTY_ID}`;
     td_PROPERTY_NAME.textContent = `${listing.LOCATION_NAME}`;
     td_PROPERTY_NAME.className = "dashboard-td";
+    td_PROPERTY_NAME.contentEditable = true;
     row.appendChild(td_PROPERTY_NAME);
 
 
@@ -98,9 +99,11 @@ function getTableRow(listing){
     td_PROPERTY_TYPE.id = `PROPERTY_TYPE-${listing.PROPERTY_ID}`;
     td_PROPERTY_TYPE.textContent = `${listing.PROPERTY_TYPE}`;
     td_PROPERTY_TYPE.className = "dashboard-td";
+    td_PROPERTY_TYPE.contentEditable = true;
     row.appendChild(td_PROPERTY_TYPE);
 
 
+    // Will add editing to images later
     const td_COVER_IMG_ID = document.createElement("td");
 
         const img = document.createElement("img");
@@ -115,10 +118,12 @@ function getTableRow(listing){
     row.appendChild(td_COVER_IMG_ID);
 
 
+
     const td_DESCRIPTION = document.createElement("td");
     td_DESCRIPTION.id = `DESCRIPTION-${listing.PROPERTY_ID}`;
     td_DESCRIPTION.textContent = `${listing.DESCRIPTION}`;
     td_DESCRIPTION.className = "dashboard-td";
+    td_DESCRIPTION.contentEditable = true;
     row.appendChild(td_DESCRIPTION);
 
 
@@ -126,6 +131,7 @@ function getTableRow(listing){
     td_ADDRESS.id = `ADDRESS-${listing.PROPERTY_ID}`;
     td_ADDRESS.textContent = `${listing.ADDRESS}`;
     td_ADDRESS.className = "dashboard-td";
+    td_ADDRESS.contentEditable = true;
     row.appendChild(td_ADDRESS);
 
 
@@ -133,6 +139,7 @@ function getTableRow(listing){
     td_LOCATION_CITY.id = `LOCATION_CITY-${listing.PROPERTY_ID}`;
     td_LOCATION_CITY.textContent = `${listing.LOCATION_CITY}`;
     td_LOCATION_CITY.className = "dashboard-td";
+    td_LOCATION_CITY.contentEditable = true;
     row.appendChild(td_LOCATION_CITY);
 
 
@@ -140,6 +147,7 @@ function getTableRow(listing){
     td_LOCATION_PROVINCE.id = `LOCATION_PROVINCE-${listing.PROPERTY_ID}`;
     td_LOCATION_PROVINCE.textContent = `${listing.LOCATION_PROVINCE}`;
     td_LOCATION_PROVINCE.className = "dashboard-td";
+    td_LOCATION_PROVINCE.contentEditable = true;
     row.appendChild(td_LOCATION_PROVINCE);
 
 
@@ -147,6 +155,7 @@ function getTableRow(listing){
     td_LOCATION_COUNTRY.id = `LOCATION_COUNTRY-${listing.PROPERTY_ID}`;
     td_LOCATION_COUNTRY.textContent = `${listing.LOCATION_COUNTRY}`;
     td_LOCATION_COUNTRY.className = "dashboard-td";
+    td_LOCATION_COUNTRY.contentEditable = true;
     row.appendChild(td_LOCATION_COUNTRY);
 
 
@@ -154,12 +163,14 @@ function getTableRow(listing){
     td_YEAR.id = `YEAR-${listing.PROPERTY_ID}`;
     td_YEAR.textContent = `${listing.YEAR}`;
     td_YEAR.className = "dashboard-td";
+    td_YEAR.contentEditable = true;
     row.appendChild(td_YEAR);
 
     const td_PARKING_COUNT = document.createElement("td");
     td_PARKING_COUNT.id = `PARKING_COUNT-${listing.PROPERTY_ID}`;
     td_PARKING_COUNT.textContent = `${listing.PARKING_COUNT}`;
     td_PARKING_COUNT.className = "dashboard-td";
+    td_PARKING_COUNT.contentEditable = true;
     row.appendChild(td_PARKING_COUNT);
 
 
@@ -167,6 +178,7 @@ function getTableRow(listing){
     td_BATH_COUNT.id = `BATH_COUNT-${listing.PROPERTY_ID}`;
     td_BATH_COUNT.textContent = `${listing.BATH_COUNT}`;
     td_BATH_COUNT.className = "dashboard-td";
+    td_BATH_COUNT.contentEditable = true;
     row.appendChild(td_BATH_COUNT);
 
 
@@ -174,6 +186,7 @@ function getTableRow(listing){
     td_ROOMS_COUNT.id = `ROOMS_COUNT-${listing.PROPERTY_ID}`;
     td_ROOMS_COUNT.textContent = `${listing.ROOMS_COUNT}`;
     td_ROOMS_COUNT.className = "dashboard-td";
+    td_ROOMS_COUNT.contentEditable = true;
     row.appendChild(td_ROOMS_COUNT);
 
 
@@ -181,6 +194,7 @@ function getTableRow(listing){
     td_PRICE.id = `PRICE-${listing.PROPERTY_ID}`;
     td_PRICE.textContent = `$${listing.PRICE}`;
     td_PRICE.className = "dashboard-td";
+    td_PRICE.contentEditable = true;
     row.appendChild(td_PRICE);
 
 
@@ -188,6 +202,7 @@ function getTableRow(listing){
     td_IS_FOR_SALE.id = `IS_FOR_SALE-${listing.PROPERTY_ID}`;
     td_IS_FOR_SALE.textContent = listing.IS_FOR_SALE === 0 ? "No" : "Yes";
     td_IS_FOR_SALE.className = "dashboard-td";
+    td_IS_FOR_SALE.contentEditable = true;
     row.appendChild(td_IS_FOR_SALE);
 
 
